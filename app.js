3e44899d0d669620b421fc996b46203a9f717a24
farman <farman@kmindz.in>
2023-11-10 11:32:33 +0530

    fixed

--- app.js
+++ app.js
@@ -15,11 +15,15 @@ const orderRouter = require("./routes/orderRouter");
 const app = express();
 
 
-const corsOption = {
+// const corsOption = {
+//   origin: ['http://localhost:5173', 'http://localhost:3000'],
+// };
+const corsOptions = {
   origin: ['http://localhost:5173', 'http://localhost:3000'],
-};
-
-app.use(cors());
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 //Golobal Middleware
 app.use(helmet());
 
