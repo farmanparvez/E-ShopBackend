51284c099a1f962c1e55f51deb0d1be42cf4d869
farman <farman@kmindz.in>
2023-12-27 21:34:03 +0530

    fixed

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -2,11 +2,14 @@ const express = require('express')
 const orderRouter = express.Router()
 const orderController = require('../controllers/orderController')
 const protect = require('../middleware/protect')
+const checkAdmin = require('../middleware/checkAdmin')
 
-orderRouter.route('/').post(protect, orderController.addOrder).get(orderController.getOrders)
+orderRouter.route('/').post(protect, checkAdmin, orderController.addOrder).get(orderController.getOrders)
 orderRouter.route('/user').get(protect, orderController.getOrderByUserId)
-orderRouter.route('/:id').get(protect, orderController.getOrderDetails).post(protect, orderController.updateOrderToDelivered)
-orderRouter.route('/:id/pay').patch(protect, orderController.updateOrderToPaid)
+orderRouter.route('/:id').get(protect, checkAdmin, orderController.getOrderDetails).post(protect, orderController.updateOrderToDelivered)
+orderRouter.route('/:id/pay').patch(protect, checkAdmin, orderController.updateOrderToPaid)
 // orderRouter.route('/').patch(protect, orderController.updateOrderToPaid)
+// admin----------------------------------------------------------------------------------------->
+// orderRouter.route('/admin').get(protect, checkAdmin, orderController.getOrderByAdminId)
 
 module.exports = orderRouter
