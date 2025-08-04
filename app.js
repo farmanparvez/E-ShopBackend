182f75bca5226712a66acca237f68041f1bd59b7
farman <farmanparvez07@gmail.com>
2025-08-04 19:21:45 +0530

    new orgin add

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 //   origin: ['http://localhost:5173', 'http://localhost:3000'],
 // };
 const corsOptions = {
-  origin: ["https://e-shop-gamma-ten.vercel.app", 'http://localhost:5173', 'http://localhost:3000', ],
+  origin: ["https://e-shop-gamma-ten.vercel.app", 'https://click-collect-navy.vercel.app', 'http://localhost:5173', 'http://localhost:3000', ],
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 200
 }
